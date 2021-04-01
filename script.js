6059618947c372c2ff94900345b801d0849f6ec6
exist08 <32675747+exist08@users.noreply.github.com>
2021-04-01 09:32:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-window.onload = () => { 
+window.onload= function(){
+
+    alert("✅Just Select The Program  \n✅and The OVR  \n✅Click on 👉🏻 MAKE CARD 👈🏻\nget the Card Animation \n\nSwitch to Dark Mode🌚 to Protect Eyes 😄");
+    const switcher=document.getElementById('switcher');
+    const body= document.body;
+
+    switcher.onclick = ()=>{
+        body.classList.toggle('dark');
+        if(body.classList.contains('dark')){
+            switcher.innerHTML="Switch to Light Mode";
+        }
+        else{
+            switcher.innerHTML="Switch to Dark Mode";
+        }
+    }
+
     var cxzw=document.getElementsByTagName("img");
-    var reuw=document.getElementById("zsc");
     var jds=0,counter=0;
     var g=setInterval(func,50);
-    reuw.onblur = ()=>{
-        cxzw[0].src=reuw.value;
-    }
+    var mc = document.getElementById('mCard');
+    var rc = document.getElementById('rCard');
     
+    var urll=`https://cdn-p2.frzdb.net/fifamobile/images/sprites_21/conv_playeranim_TT_LEGEND_1.png`;
+
+    mc.onclick=()=>{
+        var prog=document.getElementById('player-prog').value;
+        var categ=document.getElementById('player-vers').value;
+        urll=`https://cdn-p2.frzdb.net/fifamobile/images/sprites_21/conv_playeranim_${prog}_${categ}.png`;
+        cxzw[0].src=urll;
+    }
+    mc.ondblclick=()=>{
+        alert("Button is Clicked,\n Wait a moment to get the Animation Completely Loaded😊")
+    }
     function func(){
         if(counter>=60){
             counter=0;
@@ -15,6 +39,5 @@ window.onload = () => {
         cxzw[0].style=`left: ${jds}px`;
         jds-=256;
         counter++;
-        console.log(counter,jds);
     };
 };
