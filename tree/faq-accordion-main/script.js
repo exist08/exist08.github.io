864eae3a72ffc4081c56e7d04764f5ea4641ae4d
exist08 <32675747+exist08@users.noreply.github.com>
2023-12-20 00:22:02 +0530

    Add files via upload

---
+++ tree/faq-accordion-main/script.js
@@ -0,0 +1,6 @@
+// let faqHeads = document.querySelectorAll(".faq-head")
+// faqHeads.forEach((faqhead,idx)=>{
+//     faqhead.addEventListener("click",()=>{
+//         faqhead.classList.toggle("opened")
+//     })
+// })
